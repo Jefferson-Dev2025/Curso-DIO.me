a6c66b5f8edb929b100cf41cf09a14bb29895cda
JEFFERSON SAO LEAO <jeffersonsaoleao@gmail.com>
2024-11-02 12:20:24 -0300

    Meu primeiro versionamento

---
+++ Desafio Centro Pokemon.js
@@ -0,0 +1,15 @@
+console.log(Bem vindos jogadores)
+let finalizado =  cadastrado com sucesso!
+let pokeNomes = [Poochyena , Zigzagoon,Dragonite,Wurmple]
+let poketime = [
+  [Poochyena,Feminino,L05,115],
+  [Zigzagoon,Masculino,L10,100],
+  [Jefferson,Masculino,L41,1982],
+  [Dragonite,Feminino,L15,45],
+  [Wurmple,Masculino,L13,23]
+]
+console.log(Seu nome é  + poketime[2][0] +  do sexo +
+poketime[2][1] +  está no nível  + poketime[2][2] +
+ tem total de Pontos de + poketime[2][3]
+)
+console.log(poketime[2][0]+finalizado)
