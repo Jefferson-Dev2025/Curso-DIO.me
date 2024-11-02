41f00fa981c5751af3bd8edf90bcedc87f1d2c1b
JEFFERSON SAO LEAO <jeffersonsaoleao@gmail.com>
2024-11-02 18:27:39 -0300

    Create Desafio Classificador de nível de Herói.js

---
+++ Desafio Classificador de nível de Herói.js
@@ -0,0 +1,8 @@
+console.log(Bem vindos meus herois)
+let nomes = [Jefferon; Marlene; Patrick; Nick]
+TimeDeHerois=[
+[Jefferson;Azul;150];
+[Marlene;Rosa;115];
+[Patrick;Verde;250];
+[Nick;Sinza;84]   
+]
